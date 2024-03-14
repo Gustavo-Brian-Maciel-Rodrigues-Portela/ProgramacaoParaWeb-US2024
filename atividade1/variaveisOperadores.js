2efe8d9744d9ace67cd55a4b35b21da667f4ec8a
Gustavo-Brian-Maciel-Rodrigues-Portela <133496080+Gustavo-Brian-Maciel-Rodrigues-Portela@users.noreply.github.com>
2024-03-14 09:52:08 -0300

    Add files via upload

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,6 @@
+let num_A = 10, num_B = 5;
+
+console.log("Resultado da " + (num_A + num_B) + " entre " + num_A + " + " + num_B)
+console.log("Resultado da " + (num_A - num_B) + " entre " + num_A + " - " + num_B)
+console.log("Resultado da " + (num_A * num_B) + " entre " + num_A + " * " + num_B)
+console.log("Resultado da " + (num_A / num_B) + " entre " + num_A + " / " + num_B)
