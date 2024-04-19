55a0cd100603edf20558a272b79952d608926d18
Gustavo-Brian-Maciel-Rodrigues-Portela <133496080+Gustavo-Brian-Maciel-Rodrigues-Portela@users.noreply.github.com>
2024-04-18 21:25:45 -0300

    Add files via upload

---
+++ atividade5/src/estoque.js
@@ -0,0 +1,40 @@
+let produtos = [];
+
+function criarProduto(id, nome, qtd) {
+    let p = {
+        id: id,
+        nome: nome,
+        qtd: qtd
+    }
+    return p;
+}
+
+function adicionarProduto(p) {
+    produtos.push(p);
+}
+
+function listarProdutos() {
+    return produtos;
+}
+
+function removerProduto(id) {
+    produtos = produtos.filter((p) => {
+        let x = p.id != id;
+        return x;
+    });
+}
+
+function editarProduto(id, qtd) {
+    produtos = produtos.find(p => p.id === id);
+    if(produtos) {
+        produtos.qtd = qtd;
+    }
+}
+
+module.exports = {
+    criarProduto,
+    adicionarProduto,
+    listarProdutos,
+    removerProduto,
+    editarProduto
+};
